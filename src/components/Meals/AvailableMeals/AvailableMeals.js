db9aa42b0616b00c98c53878e358830059a3455f
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-31 11:18:38 +0100

    MealItem and Card

--- src/components/Meals/AvailableMeals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals/AvailableMeals.js
@@ -1,4 +1,5 @@
-import classes from './AvailableMeals.module.css'
+import classes from "./AvailableMeals.module.css";
+import Card from "../../UI/Card/Card";
 const DUMMY_MEALS = [
   {
     id: "m1",
@@ -27,15 +28,17 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    return <section className={classes.meals}>
+  return (
+    <section className={classes.meals}>
+      <Card>
         <ul>
-            { DUMMY_MEALS.map(meal => {
-                return <li>
-                    {meal.name}
-                </li>
-            })}
+          {DUMMY_MEALS.map((meal) => {
+            return <li key={meal.id}>{meal.name}</li>;
+          })}
         </ul>
+      </Card>
     </section>
+  );
 };
 
 export default AvailableMeals;
---
+++ src/components/Meals/MealItem/MealItem.js
@@ -0,0 +1,8 @@
+import Card from "../../UI/Card/Card"
+const MealItem = (props) => {
+    return <article>
+        { props.name }
+    </article>
+}
+
+export default MealItem
