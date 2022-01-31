a95aff920bf9fcfd9b3ba3dcd2c77c3dd822b099
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-01-31 11:09:16 +0100

    First components (Header, Meals, Buttons)

---
+++ src/components/Layout/Header/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import classes from "./Header.module.css";
+import mealsImage from '../../../assets/meals.jpg'
+import HeaderCardButton from "../HeaderCardButton/HeaderCardButton";
+
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>OrderApp</h1>
+        <HeaderCardButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="meal" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/MealsSummary/MealsSummary.js
@@ -0,0 +1,14 @@
+import classes from './MealsSummary.module.css'
+const MealsSummary = () => {
+    return <section className={classes.summary}>
+        <h2>Some food</h2>
+        <p>
+            Chose your favorite food meal from our broad section
+        </p>
+        <p>
+            All your meals are homemade, just-in-time and of course experience chefs
+        </p>
+    </section>
+}
+
+export default MealsSummary
