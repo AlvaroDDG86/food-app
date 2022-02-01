d9cd5fff6ff5254c693c336804fe08fe4f0747f0
AlvaroDDG86 <alvarodediosgarcia1986@gmail.com>
2022-02-01 13:10:03 +0100

    replaces useState and useEffect by useReducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,38 +1,86 @@
-import { useEffect, useState, useReducer } from "react"
-import CartContext from "./card-context"
+import { useReducer } from "react";
+import CartContext from "./card-context";
 
-const CartProvider = props => {
-    const [items, setItems] = useState([])
-    const [totalAmount, setTotalAmount] = useState(0)
-    const [totalItems, setTotalItems] = useState(0)
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+  totalItems: 0,
+};
 
-    useEffect(() => {
-        setTotalAmount(items.reduce((prev, current) => prev + (current.amount * current.price), 0).toFixed(2))
-        setTotalItems(items.reduce((prev, current) => prev + (current.amount), 0))
-    }, [items])
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      const itemsUpdated = state.items.concat(action.item);
+      return {
+        items: itemsUpdated,
+        totalAmount: itemsUpdated
+          .reduce((prev, current) => prev + current.amount * current.price, 0)
+          .toFixed(2),
+        totalItems: itemsUpdated.reduce((prev, current) => prev + current.amount, 0),
+      };
+    case "DELETE_ITEM":
+      const itemsPrevalent = state.items.filter(item => item.id !== action.id);
+      return {
+        items: itemsPrevalent,
+        totalAmount: itemsPrevalent
+          .reduce((prev, current) => prev + current.amount * current.price, 0)
+          .toFixed(2),
+        totalItems: itemsPrevalent.reduce((prev, current) => prev + current.amount, 0),
+      };
+    default:
+      return defaultCartState;
+  }
+};
 
-    const addItemHandler = item => {
-        const index = items.findIndex(meal => meal.id === item.id)
-        if (index > -1) {
+const CartProvider = (props) => {
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
 
-        } else {
-            setItems(prev => [...prev, item ])
-        }
-    }
+  // const [items, setItems] = useState([])
+  // const [totalAmount, setTotalAmount] = useState(0)
+  // const [totalItems, setTotalItems] = useState(0)
 
-    const removeItemHandler = id => {
-        setItems(prev => prev.filter(current => current.id !== id))
-    }
+  // useEffect(() => {
+  //     setTotalAmount(items.reduce((prev, current) => prev + (current.amount * current.price), 0).toFixed(2))
+  //     setTotalItems(items.reduce((prev, current) => prev + (current.amount), 0))
+  // }, [items])
 
-    return <CartContext.Provider value={{
-        items,
-        totalAmount,
-        totalItems,
+  const addItemHandler = (item) => {
+    dispatchCartAction({
+      type: "ADD_ITEM",
+      item,
+    });
+    // const index = items.findIndex(meal => meal.id === item.id)
+    // if (index > -1) {
+
+    // } else {
+    //     setItems(prev => [...prev, item ])
+    // }
+  };
+
+  const removeItemHandler = (id) => {
+    dispatchCartAction({
+      type: "DELETE_ITEM",
+      id,
+    });
+    // setItems(prev => prev.filter(current => current.id !== id))
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
+        totalItems: cartState.totalItems,
         addItem: addItemHandler,
-        removeItem: removeItemHandler
-    }}>
-        { props.children }
+        removeItem: removeItemHandler,
+      }}
+    >
+      {props.children}
     </CartContext.Provider>
-}
+  );
+};
 
-export default CartProvider
+export default CartProvider;
